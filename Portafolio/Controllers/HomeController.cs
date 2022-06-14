fbb48b62342acde748d48327010d1d4a8fd4121b
Xulio Xaviert Rojas Teixeira <xxrojas@solusoft.es>
2022-06-14 12:14:09 +0200

    añadido controladores HomeIndex

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,12 +15,43 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            
-            var persona = new Persona() { Nombre = "Xulio Xaviert",
-                Edad = 44
+            return View();
+        }
+
+        private List<Project> GetProjects()
+        {
+            return new List<Project>() { new Project
+            {
+                Title = "Proyecto 1",
+                Description = "Web Page ASP.NET",
+                Link = "htpps://www.google.es",
+                ImgURL = "https://images.pexels.com/photos/270404/pexels-photo-270404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            },
+            new Project
+            {
+                Title = "Proyecto 2",
+                Description = "Web Page ASP.NET",
+                Link = "htpps://www.google.es",
+                ImgURL = "https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            },
+
+            new Project
+            {
+                Title = "Proyecto 3",
+                Description = "Web Page ASP.NET",
+                Link = "htpps://www.google.es",
+                ImgURL = "https://images.pexels.com/photos/270360/pexels-photo-270360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            },
+
+            new Project
+            {
+                Title = "Proyecto 4",
+                Description = "Web Page ASP.NET",
+                Link = "htpps://www.google.es",
+                ImgURL = "https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            },
+
             };
-            
-            return View(persona);
         }
 
         public IActionResult Privacy()
