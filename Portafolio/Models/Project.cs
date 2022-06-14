43b13f734260837251b0fe643f2e312c4e2ff2ea
Xulio Xaviert Rojas Teixeira <xxrojas@solusoft.es>
2022-06-14 11:32:55 +0200

    primera parte del portfolio

---
+++ Portafolio/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Portafolio.Models
+{
+    public class Project
+
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImgURL { get; set; }
+        public string Link { get; set; }
+
+    }
+}
